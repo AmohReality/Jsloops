9c8da3140becc8416dfa2ad7c13a4d3e1e6b1f13
Marian <amohreality@gmail.com>
2021-08-11 06:46:19 +0200

    I have completed the exercies

--- tod.js
+++ tod.js
@@ -104,6 +104,7 @@ console.log(arr[i])
 */
 
 
+/*
 //Exercise 4:Adding elements in the same position
 let arrA=[3,9,4]
 let arrB=[6,1,8]
@@ -115,6 +116,169 @@ sum=arrA[i]+arrB[i]
 sum_arr.push(sum)
 }
 console.log(sum_arr)
+*/
+
+
+//***********
+//app.all('path', (req, res, next) => {    
+//});
+//**********
+
+
+/*
+//Exercise 5: Making every second item of a string uppercase
+let names='oluwadara';
+for(i=0;i<names.length;i++)
+{
+    if (i%2 !=0)
+    {
+        console.log (names[i].toUpperCase());
+    }
+    else
+    {
+     console.log(names[i])
+    }
+}
+*/
+
+
+
+/*
+//Exercise 6: Check if a string contains letter 'y', if it does pring yes, otherwise, print 'no'
+//###### Check it again
+let text = 'I did not see you';
+let x=""
+for(i=0;i<text.length;i++)
+{
+    if(text[i]==='y')
+    {
+        x='yes'
+    }
+    else
+    {
+        x='no'
+    }
+    console.log(x)
+}
+*/
+
+
+/*
+//Exercise 7: Given a number n, calculate the factorial of the number
+let n = 4;
+let x=1;
+for(i=n;i>0;i--)
+{
+ x*=i;
+}
+console.log(x) 
+*/
+
+/*
+//Exercise 8: Allows the user to guess a 4-digit pit exactly four times
+let pin= 4367;
+let access= 4367
+let y='please make your guess';
+let x= 'that was correct!'
+for(i=0;i<4;i++)
+{
+    if(pin===access)
+    {
+        console.log(x)
+    }
+    else
+    {
+        console.log(y)
+    }
+
+}
+*/
+
+
+//Exercise 9:Find whether a string is a palindrome, i.e reads the same forward and backwork
+/*
+let str= "nun";
+const stringy= ()=>
+{
+let splittyarr = str.split('')
+let splitreverse= splittyarr.reverse();
+let joinsplre= splitreverse.join('')
+return joinsplre;
+}
+for(i=0;i<str.length;i++)
+{
+    if(str[i]===stringy()[i])
+    {
+        console.log("that's a palindrome")
+    }
+    else
+    {
+        "That's not a palindrome"
+    }
+}
+
+
+
+
+//Better still:
+
+let word ='nurses' ;
+let wordr='';
+function myword()
+{
+for(let i=0;i<word.length;i++)
+{
+    let x=word.split(word[i]);
+    let y=x.reverse();
+    wordr=y.join();  
+}
+return wordr;
+}
+if(word===myword())
+{
+    console.log("That's is a palindrome")
+}
+else
+{
+    console.log('Not a palindrome')
+}
+
+
+
+
+
+//Another way
+
+let word= 'racecar';
+const workout= ()=>{
+let reverse_word= '';
+for(let i=word.length-1;i>=0;i-- )
+{
+   reverse_word+= word[i];
+}
+return reverse_word;
+}
+if(word===workout())
+{
+    console.log("That's is a palindrome")
+}
+else
+{
+    console.log('Not a palindrome')
+}
+*/
+
+
+
+
+//Exercise 10;Given a num, do the summation from 1 to that number
+let num=2;
+let sum=0;
+for (i=0;i<=num;i++)
+{
+   sum+=i
+}
+console.log(sum)
 
 
 
